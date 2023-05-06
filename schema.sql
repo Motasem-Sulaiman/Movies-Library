ba4042e60f991664fe75421214b65f35e8dee79c
Motasem-Sulaiman <motasemismael0@gmail.com>
2023-05-06 22:39:29 +0000

    edit

--- schema.sql
+++ schema.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS addmovie(
 id SERIAL PRIMARY KEY,
 title varchar(255),
 release_date varchar(255),
-poster_path varchar(255)
+poster_path varchar(255),
+comments varchar(255)
 
 );
